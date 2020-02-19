9b14282a1288bdd199aec79893c009d01cc92c97
nu50218 <nu_ll@icloud.com>
2020-02-19 11:03:01 +0900

    add FromResponse

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/nu50218/nucredit
 
 go 1.13
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1
+	github.com/nu50218/go-nagoyau v0.0.0-20200114160302-2a7c8ac8989d
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
+	golang.org/x/text v0.3.0
+)
--- subject.go
+++ subject.go
@@ -3,7 +3,14 @@ package nucredit
 import (
 	"encoding/json"
 	"io"
+	"log"
+	"net/http"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/text/width"
 )
 
 // Subject 科目
@@ -27,7 +34,7 @@ type Subject struct {
 // Subjects 科目列
 type Subjects []*Subject
 
-// FromReader Readerから読んでSubjectsにして返す
+// FromReader Readerからjson形式で読んでSubjectsにして返す
 func FromReader(r io.Reader) (Subjects, error) {
 	s := Subjects{}
 	if err := json.NewDecoder(r).Decode(&s); err != nil {
@@ -43,6 +50,82 @@ func FromFile(filename string) (Subjects, error) {
 		return nil, err
 	}
 	return FromReader(f)
+}
+
+// FromResponse 成績ページをパースしてSubjectsにして返す
+func FromResponse(res *http.Response) (Subjects, error) {
+	doc, err := goquery.NewDocumentFromResponse(res)
+	if err != nil {
+		return nil, err
+	}
+
+	subjects := Subjects{}
+
+	// パース
+	log.Println(doc.Text())
+	table := doc.Find("table").Eq(1)
+	log.Println(table.Text())
+	body := table.Find("tbody").Eq(0)
+	log.Println(body.Text())
+	data := body.Find("tr")
+	log.Println(data.Text())
+
+	// 【科目区分 】 -> 科目区分
+	categoryFmt := func(s string) string {
+		s = strings.TrimLeft(s, "【")
+		s = strings.TrimRight(s, "】")
+		return strings.TrimSpace(s)
+	}
+
+	// dataを処理していく
+	// TODO: refactor
+
+	var currentCategory string
+
+	data.EachWithBreak(func(i int, s *goquery.Selection) bool {
+		// カラム名はスキップ
+		if i == 0 {
+			return true
+		}
+
+		td := s.Find("td")
+
+		// 科目区分の行 背景の色で判定
+		if td.Eq(0).AttrOr("bgColor", "white") != "white" {
+			currentCategory = categoryFmt(td.Eq(0).Text())
+			return true
+		}
+
+		// 通常の行
+		subject := &Subject{
+			Category: currentCategory,
+			Name:     td.Eq(0).Text(),
+			Teacher:  td.Eq(1).Text(),
+			// 半角にする
+			Grade: width.Narrow.String(td.Eq(3).Text()),
+		}
+
+		if subject.Credit, err = strconv.ParseFloat(td.Eq(2).Text(), 64); err != nil {
+			return false
+		}
+
+		term := strings.Split(td.Eq(4).Text(), " ")
+		// 単位が出ていないと存在しないのでうまくパースできたときだけ追加する
+		if len(term) == 2 {
+			if subject.Year, err = strconv.Atoi(term[0]); err != nil {
+				return false
+			}
+			subject.Semester = term[1]
+		}
+
+		subjects = append(subjects, subject)
+		return true
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return subjects, nil
 }
 
 // FilterFunc フィルタリングの条件
